cfeb782cd6867551bad500bc114d48acef65d9bc
Monalisa Onyango <+>
2024-09-02 22:52:54 +0300

    inheritance1

--- lib/student.py
+++ lib/student.py
@@ -1,8 +1,22 @@
 #!/usr/bin/env python
 
+
+
 from user import User
 
 class Student(User):
+
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge =[]
     
-    def learn(self):
-        pass
+    def learn(self,learn_string):
+        self.knowledge.append(learn_string)
+        return learn_string    
+    
+
+student = Student("Monalisa","Sabina")
+
+student.learn("cassava farming")
+print(student.knowledge)
+print(f"{student.first_name} {student.last_name} wants to learn {student.knowledge}")
